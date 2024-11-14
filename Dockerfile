b65a6c04575d29840523ce0dc8bdfca8d5152dee
MartinH80 <martin@herrman.nl>
2024-11-14 22:44:06 +0100

    Initial Dockerfile and startup.sh

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM alpine:3.20.3@sha256:1e42bbe2508154c9126d48c2b8a75420c3544343bf86fd041fb7527e017a4b4a
+
+MAINTAINER MartinHerrman martin@herrman.nl
+
+WORKDIR /
+
+# update
+RUN apk update
+RUN apk upgrade
+
+# install openssh-server
+RUN apk add openssh-sftp-server
+
+# make directory to expose user folders
+RUN mkdir /data
+
+# copy startup script
+ADD startup.sh /startup.sh
+
+# Execute startup script
+CMD ["/bin/sh","-c","/startup.sh"]
---
+++ startup.sh
@@ -0,0 +1,33 @@
+# start syslog daemon
+/sbin/syslogd
+
+# create sftp group
+addgroup sftpg
+
+# Create required users and their personal directory
+# Read each line from /data/userlist.txt
+while IFS=' ' read -r username password; do
+  # Check if both username and password are not empty
+  if [ -n "$username" ] && [ -n "$password" ]; then
+    # create user
+    #   without home directory (-H)
+    #   without asking for a password (-D)
+    #   without shell access (-s /sbin/nologin)
+    #   in group sftpg
+    adduser -H -D -s /sbin/nologin $username sftpg
+    # Set the user's password
+    echo "$username:$password" | chpasswd
+    echo "Created user $username with specified password."
+    # make user directory
+    mkdir /data/$username
+    chown root:sftpg /data/$username
+  else
+    echo "Skipping line: either username or password is missing."
+  fi
+done < /data/userlist.txt
+
+# create ssh keys
+/usr/bin/ssh-keygen -A
+
+# start sshd service
+/usr/sbin/sshd -D
